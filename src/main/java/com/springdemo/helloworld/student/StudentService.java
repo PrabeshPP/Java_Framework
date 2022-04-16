04a9aeb7cc4d9a9ce8860b238c5bd4688cf8af47
Prabesh <prabesh.bista369@gmail.com>
2022-04-16 18:47:45 +0545

    impl. loose coupling

--- src/main/java/com/springdemo/helloworld/student/StudentService.java
+++ src/main/java/com/springdemo/helloworld/student/StudentService.java
@@ -1,8 +1,10 @@
 package com.springdemo.helloworld.student;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.util.List;
-
+@Service
 public class StudentService {
     public List<Student> getStudents(){
         return List.of(
