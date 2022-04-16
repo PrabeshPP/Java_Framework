9797f750e5fc4ada6b7a317ce5b7609f43f8e4f3
Prabesh <prabesh.bista369@gmail.com>
2022-04-16 16:24:04 +0545

    Student Class

--- src/main/java/com/springdemo/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springdemo/helloworld/HelloWorldApplication.java
@@ -2,12 +2,23 @@ package com.springdemo.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(HelloWorldApplication.class, args);
     }
+    @GetMapping
+    public List<String> hello(){
+        return List.of("Prabesh","Bista");
+    }
+
 
 }
---
+++ src/main/java/com/springdemo/helloworld/student/Student.java
@@ -0,0 +1,12 @@
+package com.springdemo.helloworld.student;
+
+import java.util.Date;
+
+public class Student {
+    private Long id;
+    private  String name;
+    private  int age;
+    private Date date;
+    private  String email;
+
+}
