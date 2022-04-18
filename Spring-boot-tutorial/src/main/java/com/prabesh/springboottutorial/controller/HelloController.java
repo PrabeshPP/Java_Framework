a01eb72debb34076d18eb8f25f65fa9b890441f3
Prabesh <prabesh.bista369@gmail.com>
2022-04-18 09:58:06 +0545

    Simple Rest Api

---
+++ Spring-boot-tutorial/src/main/java/com/prabesh/springboottutorial/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.prabesh.springboottutorial.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String helloWorld(){
+        return "Welcome to the new world of Spring!!";
+    }
+
+}
